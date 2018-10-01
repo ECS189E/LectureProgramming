e2960124ff31a4b4f8acba3c3143d64d174471b3
Sam King <kingst@gmail.com>
2018-10-01 16:44:09 -0700

    first commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startClick() {
+        print("startClick")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
