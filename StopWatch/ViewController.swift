eee0379c68c01fbc182562b080ae27e1598a32aa
Sam King <kingst@gmail.com>
2018-10-01 09:19:28 -0700

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Sam King on 10/1/18.
+//  Copyright © 2018 Sam King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
