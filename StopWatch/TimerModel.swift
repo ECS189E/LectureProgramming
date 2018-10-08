f37a5757d909537d5a34ad2e19898739c57de921
Sam King <kingst@gmail.com>
2018-10-08 16:54:14 -0700

    working on next lecture

--- StopWatch/TimerModel.swift
+++ StopWatch/TimerModel.swift
@@ -10,30 +10,61 @@ import Foundation
 
 protocol UpdateTime {
     func timeUpdated(_ interval: TimeInterval)
+    func lapsUpdated(_ laps: [TimeInterval])
 }
 
 class TimerModel {
     var updateDelegate: UpdateTime?
     var startTime: Date?
+    var stopTime: Date?
+    var lastLap: Date?
     var timer: Timer?
+    var laps: [TimeInterval]
     
     init() {
+        self.laps = []
     }
     
-    func timeSinceStart() -> TimeInterval {
+    func timeSinceStart(_ date: Date = Date()) -> TimeInterval {
         guard let startTime = self.startTime else {
             print("something is wrong")
             return 0.0
         }
         
-        return Date().timeIntervalSince(startTime)
+        return date.timeIntervalSince(startTime)
     }
     
-    func startTimer() -> Void {
+    func stop() {
+        let stopTime = Date()
+        self.stopTime = stopTime
+        self.timer.map { $0.invalidate() }
+        self.timer = nil
+        self.updateDelegate.map { $0.timeUpdated(self.timeSinceStart(stopTime)) }
+    }
+    
+    func lap() {
+        guard let startTime = self.startTime else {
+            print("set lap when not running, ignore")
+            return
+        }
+        
+        let now = Date()
+        let lapTime = now.timeIntervalSince(self.lastLap ?? startTime)
+        self.laps.insert(lapTime, at: 0)
+        self.lastLap = now
+        self.updateDelegate.map { $0.lapsUpdated(self.laps) }
+    }
+    
+    func start() {
         self.startTime = Date()
-        self.timer = Timer(timeInterval: 0.01, repeats: true, block: { (_) in
+        let timer = Timer(timeInterval: 0.01, repeats: true, block: { (_) in
             let elapsedTime = self.timeSinceStart()
             self.updateDelegate.map { $0.timeUpdated(elapsedTime) }
         })
+        RunLoop.current.add(timer, forMode: .default)
+        self.timer = timer
+        self.laps = []
+        
+        self.updateDelegate.map { $0.lapsUpdated(self.laps) }
     }
 }
--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,19 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController, UpdateTime {
+class ViewController: UIViewController, UpdateTime, UITableViewDelegate, UITableViewDataSource {
+    struct LapData {
+        let number: Int
+        let time: Double
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.laps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lap") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "lap")
+        let lap = self.laps[indexPath.row]
+        cell.textLabel?.text = "Lap \(lap.number)"
+        cell.detailTextLabel?.text = String(format: "%0.02f", lap.time)
+        
+        return cell
+    }
+    
+    func lapsUpdated(_ laps: [TimeInterval]) {
+        let count = laps.count
+        self.laps = []
+        for (idx, lap) in laps.enumerated() {
+            self.laps.append(LapData(number: (count - idx), time: lap))
+        }
+        self.tableView.reloadData()
+    }
+    
     func timeUpdated(_ interval: TimeInterval) {
-        // set our label
+        self.elapsedTimeLabel.text = String(format: "%0.02f", interval)
     }
     
 
     let timer = TimerModel()
+    var laps: [LapData] = []
     
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func lapClick() {
+        self.timer.lap()
+    }
     
     @IBAction func startClick() {
+        if self.startButton.currentTitle == "Start" {
+            self.timer.start()
+        } else {
+            self.timer.stop()
+        }
         self.toggleButton(withTitles: ("Start", "Stop"), on: self.startButton)
-        self.timer.startTimer()
     }
     
     func toggleButton(withTitles titles: (String, String), on button: UIButton) {
